c03fc42977a5d685b5b1294698a5c993996664e8
space-mp4-exe <brentbristol3@gmail.com>
2023-09-12 12:26:32 -0500

    changed demo

--- Data Structures/Demo.cpp
+++ Data Structures/Demo.cpp
@@ -1,24 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <list>
 using namespace std;
 
 int main(){
-	int *p = new int[2];
-	int *q = new int[2];
-
-	p[0] = 4;
-	p[1] = 5;
-
-	q[0] = 2;
-	q[1] = 3;
-
-	p[0] = q[0];
-	p[1] = q[1];
-
-	cout << p[0] << endl;
-
-	cout << p[1] << endl;
-
-	delete []p;
-	delete []q;
+	Node a;
 }
