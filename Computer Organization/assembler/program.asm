f98dcee5b2b1d2e31b57cb8d371b68d06d841bad
space-mp4-exe <you@example.com>
2024-04-05 13:39:14 -0500

    added divide

--- Computer Organization/assembler/program.asm
+++ Computer Organization/assembler/program.asm
@@ -1,16 +1,22 @@
             ORG     10
-DIVIDE,     STA     DIVIDEND    //store value of last loop
-            //NOP               Body of while loop goes here
+DIVIDE,     LDA     DIVIDEND    //To use, set values for DIVIDEND and DIVISOR. After completion, quotient is in QUO and remainder is in DIVIDEND.
+DIVIDEL,    STA     DIVIDEND    //store value of last loop
+            LDA     QUO         //load quotient into AC
+            INC                 
+            STA     QUO         //QUO++
             LDA     DIVISOR     //move divisor into AC
             CMA                 //complement divisor
             INC                 //Divisor is now negative
             ADD     DIVIDEND    //Subtract Dividend from Divisor
-            STA     RES
             SNA                 //If Dividend is larger than Divisor, this number will be positive
-            BUN     DIVIDE
+            BUN     DIVIDEL     //internal label for looping, end of loop. while(Dividend>=Divisor){};
+            LDA     QUO         //quotient is 1 more than actual value
+            ADD     MIN1        //subtract 1
+            STA     QUO
             HLT
 
 DIVIDEND,   DEC     22
 DIVISOR,    DEC     4
-RES,        DEC     0
+QUO,        DEC     0
+MIN1,       DEC     -1
             END
