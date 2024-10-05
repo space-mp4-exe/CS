e7b454082d13d9efe054140a730f81ab9aed850d
space-mp4-exe <brentbristol3@gmail.com>
2024-10-05 23:31:50 +0000

    finished project

--- Assignments/project1.c
+++ Assignments/project1.c
@@ -1,25 +1,24 @@
+//Brent Bristol 4x4: bris0040 username: cs0015
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 
-int* total;
-void *runner(void *param);//threads for processes to run on
+int* total;//shared variable
+void incTotal(int count);//increment total variable
 
 int main(int argc, char *argv[]){
 	//set up shared memory
 	int shmID;//id of shared memory segment
 	char* shmAddr;//address of shared memory segment
 	struct shmid_ds shm_desc;
-	
+
 	//allocate shared memory segment
 	shmID = shmget(100, 2048, IPC_CREAT | IPC_EXCL | 0600);
 	if(shmID == -1){//operation failed
-		//shmctl(shmID, IPC_RMID, &shm_desc);
 		perror("main: shmget: ");
 		exit(1);
 	}
@@ -31,32 +30,41 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	total = (int*) shmAddr;
-	*total = 0;
+	total = (int*) shmAddr;//set total to shared memory location
 
-	//set up threads
-	pthread_t tid1; //thread identifier
-	pthread_t tid2;
-	pthread_t tid3;
-	pthread_t tid4;
-	pthread_attr_t attr; //thread attributes
-	pthread_attr_init(&attr);//get default attributes
-	
-	//make 4 children threads
-	pthread_create(&tid1, &attr, runner, (int*) 1);
-	pthread_create(&tid2, &attr, runner, (int*) 2);
-	pthread_create(&tid3, &attr, runner, (int*) 3);
-	pthread_create(&tid4, &attr, runner, (int*) 5);
+	pid_t pid1, pid2, pid3, pid4;//id's of child processes
 
-	//wait for children to finish executing
-	pthread_join(tid1,NULL);
-	printf("total count: %d\n",*total);
-	pthread_join(tid2,NULL);
-	printf("total count: %d\n",*total);
-	pthread_join(tid3,NULL);
-	printf("total count: %d\n",*total);
-	pthread_join(tid4,NULL);
-	printf("total count: %d\n",*total);
+	//run each of the children
+	if((pid1 = fork()) == 0){
+		incTotal(100000);
+		exit(0);
+	}
+	else if((pid2 = fork()) == 0){
+		incTotal(200000);
+		exit(0);
+	}
+	else if((pid3 = fork()) == 0){
+		incTotal(300000);
+		exit(0);
+	}
+	else if((pid4 = fork()) == 0){
+		incTotal(500000);
+		exit(0);
+	}
+
+	//wait for the children to finish running 
+	wait(NULL);
+	printf("total value: %d\n", *total);
+	printf("child with ID:%d just exited\n", pid1);
+	wait(NULL);
+	printf("total value: %d\n", *total);
+	printf("child with ID:%d just exited\n", pid2);
+	wait(NULL);
+	printf("total value: %d\n", *total);
+	printf("child with ID:%d just exited\n", pid3);
+	wait(NULL);
+	printf("total value: %d\n", *total);
+	printf("child with ID:%d just exited\n", pid4);
 
 	//detach shared memory from address space
 	if(shmdt(shmAddr) == -1){
@@ -67,15 +75,15 @@ int main(int argc, char *argv[]){
 	if(shmctl(shmID, IPC_RMID, &shm_desc) == -1){
 		perror("main: shmctl: ");
 	}
+
+	//end of program statement
+	printf("End of Program\n");
 	exit(0);
 }
 
-void *runner(void *param) {
-	int k = atoi(param);
-	for(int i = 0; i < (k * 100000); i++){
+void incTotal(int count){
+	for(int i = 0; *total < count; i++){
 		(*total)++;
 	}
-
-	pthread_exit(0);
 }
 
