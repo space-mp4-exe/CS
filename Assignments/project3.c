d6863cd1b2686c3efb9638637d7e990a0d03e630
space-mp4-exe <brentbristol3@gmail.com>
2024-11-27 00:21:33 +0000

    finished project3

--- Assignments/project3.c
+++ Assignments/project3.c
@@ -10,6 +10,7 @@
 #include <semaphore.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdlib.h>
 #define BUFFER_SIZE 15
 
 void reader();
@@ -19,33 +20,17 @@ void printer();
 typedef struct{
 	char character;
 }element;
-//struct for semaphores in shared memory
-typedef struct{
-	sem_t semBuf;
-}sharedMem;
+
 element buffer[BUFFER_SIZE];
 int inFlag = 0;
 int outFlag = 0;
 
-sem_t semBuf;//semaphore for managing buffer access
+char* name = "Buffer";//semaphore name
 
 int main(){
-	//create semaphore in shared memory
-	char* name = "Semaphore";
-	int fd;
-	fd = shm_open(name, O_EXCL | O_CREAT | O_RDWR, 0666);
-	if(fd == -1){//error message
-		fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-		shm_unlink(name);
-		perror("main: shm_open: ");
-		exit(1);
-	}
-	ftruncate(fd, sizeof(sharedVar));
-	//map struct to shared memory
-	sharedMem = mmap(NULL, sizeof(sharedVar), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	//create named semaphore
+	sem_t* semBuf = sem_open(name, O_EXCL | O_CREAT | O_RDWR, 0666);
 
-	sem_init(&sharedMem->semBuff, 1, 1);//initialize semaphorek
-	
 	pthread_t tid1[1]; /* process id for thread 1 */
 	pthread_t tid2[1]; /* process id for thread 2 */
 	pthread_attr_t attr[1]; /* attribute pointer array */
@@ -67,10 +52,10 @@ int main(){
 	pthread_exit(NULL);
 	
 	//deallocate semaphore
-	sem_destroy(&sharedMem->sem);
+	sem_destroy(semBuf);
 
 	//unlink shared memory
-	shm_unlink(name);
+	sem_unlink(name);
 
 	return 0;
 }
@@ -81,6 +66,7 @@ void reader(){
 	fp = fopen("mytest.dat", "r");
 	element next_produced;
 
+	sem_t* semBuf = sem_open(name, 0);
 	while (fscanf(fp,"%c",&newChar) != EOF) {
 		// Generate new item
 		next_produced.character = newChar;
@@ -88,31 +74,33 @@ void reader(){
 		// Wait for there to be space in the buffer
 		while (((inFlag + 1) % BUFFER_SIZE) == outFlag); /* do nothing */
 		// Place item in the buffer
-		sem_wait(&semBuf);
+		sem_wait(semBuf);
 		buffer[inFlag] = next_produced;
-		sem_post(&semBuf);
+		sem_post(semBuf);
 		inFlag = (inFlag + 1) % BUFFER_SIZE;
 	}
 	//add end of file character to buffer
 	next_produced.character = '*';
-	sem_wait(&semBuf);
+	sem_wait(semBuf);
 	buffer[inFlag] = next_produced;
-	sem_post(&semBuf);
+	sem_post(semBuf);
 
 	fclose(fp);
 }
 
 void printer(){
 	element next_consumed;
-	//sem_wait(&semBuf);//shouldn't run before there is something in the buffer
+	sem_t* semBuf = sem_open(name, 0);
+	sem_wait(semBuf);//shouldn't run before there is something in the buffer
+
 	while (buffer[outFlag].character != '*') {
 		// Wait for item to be available
 		while (inFlag == outFlag); /* do nothing */
 
 		// Get the next item
-		sem_wait(&semBuf);
+		sem_wait(semBuf);
 		next_consumed = buffer[outFlag];
-		sem_post(&semBuf);
+		sem_post(semBuf);
 		printf("%c",next_consumed.character);
 		fflush(stdout);
 		outFlag = (outFlag + 1) % BUFFER_SIZE;
